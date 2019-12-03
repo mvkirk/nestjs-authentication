f7dc990f976ff710bccaad5a5cd01771b720a4d0
Maël Vincent <mael.vincent.pro@gmail.com>
2019-12-03 09:29:13 +0100

    Guard users routes

--- README.md
+++ README.md
@@ -40,3 +40,43 @@ $ npm run test:e2e
 # test coverage
 $ npm run test:cov
 ```
+
+## Routes
+
+### Authentication
+
+```
+POST /auth/register
+{
+  "email": "example@domain.com",
+  "password": "azerty"
+}
+```
+
+```
+REQUEST
+
+POST /auth/login
+{
+  "email": "example@domain.com",
+  "password": "azerty"
+}
+
+Response
+{
+  "expires_in": 3600,
+  "access_token": "ACCESS_TOKEN",
+  "user_id": 1,
+  "status": 200
+}
+```
+
+### Users
+
+`GET /users/:id`
+
+`POST /users`
+
+`PUT /users`
+
+`DELETE /users/:id`
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,5 @@
-
-import { Controller, Post, Body, Get, Put, Delete,Param} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Controller, Post, Body, Get, Put, Delete,Param, UseGuards} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 
@@ -8,21 +8,25 @@ export class UsersController {
 
     constructor(private service: UsersService) { }
 
+    @UseGuards(AuthGuard('jwt'))
     @Get(':id')
     get(@Param() params) {
         return this.service.getUser(params.id);
     }
 
+    @UseGuards(AuthGuard('jwt'))
     @Post()
     create(@Body() user: User) {
         return this.service.createUser(user);
     }
 
+    @UseGuards(AuthGuard('jwt'))
     @Put()
     update(@Body() user: User) {
         return this.service.updateUser(user);
     }
 
+    @UseGuards(AuthGuard('jwt'))
     @Delete(':id')
     deleteUser(@Param() params) {
         return this.service.deleteUser(params.id);
